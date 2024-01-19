2cde910bb58c3e71632feb3c3db4afed2a375e88
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-19 17:34:00 -0400

    fizzbuzz

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(s):
     for char in s:
-        print("{}".format(chr(ord(char) - 32) 
-                          if 97 <= ord(char) <= 122 else char), end="")
-    print()
+        print("{}".format(chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char), end="")
+        print()
