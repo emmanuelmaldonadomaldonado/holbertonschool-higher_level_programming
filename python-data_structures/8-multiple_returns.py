742fad667ec19891c085f374e5bdf31b07dffe21
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-23 15:02:03 -0400

    returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    s_len = len(sentence)
-    if s_len == 0:
-        f_char = None
-    else:
-        f_char = sentence[0]
-        return ((s_len, f_char))
+    if not sentence:
+        return (0, None)
+
+    return (len(sentence), sentence[0])
