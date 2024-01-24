64faa954c8d0e089f95fba6605c705d1ec480dba
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-24 11:37:00 -0400

    nose

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure each tuple has at least 2 elements, filling with 0 if needed
-    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
-    tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
-
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    print(tuple_a)
+    print(tuple_b)
     # Create a new tuple with the sum of corresponding elements
     result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
