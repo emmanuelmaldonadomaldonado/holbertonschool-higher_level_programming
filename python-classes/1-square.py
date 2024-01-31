c8a8eb16feb1004d9f319595b1eaf27d9b14af8e
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-31 12:38:47 -0400

    __priv

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Create parapeters
+
+        Args:
+            size (int): the size
+        """
+        self.__size = size
