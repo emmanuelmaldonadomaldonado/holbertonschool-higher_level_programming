2020760cbaa09cdf78ec4899c6bb2c07698060df
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-31 13:39:24 -0400

    isins

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -15,4 +15,6 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+    def area(self): 
+        """Check for area"""  
+        return self.__size * self.__size
