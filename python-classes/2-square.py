28410652d069b053949e7be4f1d7332bd6dfe650
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-31 13:34:41 -0400

    isins

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,17 +6,13 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Create parapeters
+        """Initialize a new Square.
 
         Args:
-            size (int): the size
+            size (int): The size of the new square.
         """
-        if not isinstance(self, size=0):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """Check for area"""
-        return self.__size * self.__size
