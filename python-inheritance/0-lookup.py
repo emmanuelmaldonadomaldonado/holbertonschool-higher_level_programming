6cc5d37deaf31a34af09b941f1239d96b5a7a396
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-02-07 10:22:37 -0400

    look

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""Defines an object"""
-
-
 def lookup(obj):
-   """
-   Returns a list of available attributes and methods of an object"""
    list = dir(obj) 
    return list
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class MyList:
+    """Sort a list"""
+    def print_sorted(self):
+        print(sorted(self))
