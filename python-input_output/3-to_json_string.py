d735ec77fb63c60c94660aa6f1a7e07a96bdc29a
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-02-09 10:37:24 -0400

    read

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a function"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
+
+    Args:
+        my_obj: object
+
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    return json.dumps(my_obj)
