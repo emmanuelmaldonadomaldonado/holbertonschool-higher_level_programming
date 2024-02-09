a914ad7284d71c74c44929785d34ca180bec0a35
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-02-09 09:54:27 -0400

    read

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads from a file """
+""" Module that contains a function that appends to a text file
+"""
 
 
-def write_file(filename="", text=""):
-    """ Function that reads from a file
+def append_write(filename="", text=""):
+    """ Function that appends to a text file
 
     Args:
         filename: filename
+        text: text to write
 
     Raises
         Exception: when the file can be opened
