75c6da198eaf95d21bf39c36c464770b5afa824f
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-03-07 12:19:14 -0400

    IS NOT NULL

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- Script that lists all records of the table second_table of the database hbtn_0c_0
-SELECT name
+SELECT score, name
 FROM second_table
-WHERE name IS NOT NULL;
+WHERE name IS NOT NULL
+ORDER BY score DESC;
