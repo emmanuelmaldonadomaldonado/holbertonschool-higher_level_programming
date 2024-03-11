d068ad284cb2aaf63ce4cb41e6b7ada882e7bd07
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-03-11 15:51:50 -0400

    f

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-script that lists all states from the database hbtn_0e_0_usa
-"""
+# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+
 import sys
 import MySQLdb
 
