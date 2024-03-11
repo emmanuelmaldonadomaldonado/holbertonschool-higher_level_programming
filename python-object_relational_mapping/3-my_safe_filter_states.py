776a70699da62e1667d9526674c5380794b65e32
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-03-11 16:20:54 -0400

    %s

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-script that lists all states from the database hbtn_0e_0_usa
+Lists all values in the states tables of a database where name
+matches the argument in a safe way
 """
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    
-    state_name = sys.argv[4]
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id;".format(state_name))
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
     states = cur.fetchall()
 
     for state in states:
