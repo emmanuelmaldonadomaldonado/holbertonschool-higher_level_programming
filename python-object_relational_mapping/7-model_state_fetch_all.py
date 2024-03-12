253e332513d209aff84e9f9795aa3d7d584d8374
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-03-12 11:53:11 -0400

    .

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,7 @@ import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-  
+
 if __name__ == '__main__':
     
     username = sys.argv[1]
@@ -18,6 +18,7 @@ if __name__ == '__main__':
                            format(username, password, database))
 
     Session = sessionmaker(bind=engine)
+    
     session = Session()
     
     states = session.query(State).order_by(State.id).all()
